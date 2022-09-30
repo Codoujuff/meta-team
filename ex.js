0f3c68d4ad90d74c211fb96d5b66442b5e21c81b
codou <106439667+Codoujuff@users.noreply.github.com>
2022-10-01 00:28:03 +0200

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,13 @@
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+let con = document.getElementById("con")
+let accueil = document.getElementById("accueil")
+
+email.addEventListener("submit",(event)=>{ //pass automatiquement , addEventListener a deux parametres ,les input prennent des value
+  even.preventDefault() //te previent de remplir correctement,    required pour verifier que le champ n'est pas vide
+})
+
+password.addEventListener("submit",(event)=>{
+  even.preventDefault()
+})
+                                                                                             
